7b54195a0029e0b2ca661af33c04bcb50d0b37fa
c030110l <55715470+c030110l@users.noreply.github.com>
2020-10-24 16:39:24 -0700

    Add files via upload

---
+++ review assignment 1/question2.java
@@ -0,0 +1,17 @@
+class question2{
+
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int sum=0;
+
+        for(int i=0; i<4000000;i=a+b){
+            if(i%2==0){
+                sum+=1;    
+            }
+            a=b;
+            b=i;
+        }
+        System.out.println(sum);
+    }
+}
